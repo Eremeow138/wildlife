7b91143b12bb78547a214639f16e1effd6f0af71
Eremeow138 <eremeow138@yandex.ru>
2021-02-03 13:42:09 +0800

    fix: bug when the navbar is open and width increases

--- js/toggle.js
+++ js/toggle.js
@@ -8,10 +8,25 @@ const main = document.querySelector('.main_content');
 
 const logotype = document.querySelector('.logotype');
 
-toggle.addEventListener('click', () => {
+let isOpen = false;
+
+function test() {
   navbar.classList.toggle('slide-left');
   footer.classList.toggle('slide-left');
   main.classList.toggle('slide-left');
   logotype.classList.toggle('slide-left');
   toggle.classList.toggle('toggle_clicked');
+  if (isOpen) {
+    isOpen = false;
+  } else {
+    isOpen = true;
+  }
+}
+
+toggle.addEventListener('click', test);
+
+window.addEventListener('resize', () => {
+  if (document.documentElement.clientWidth > 767 && isOpen) {
+    test();
+  }
 });
